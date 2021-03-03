803e5d1474dabcd20922e6238f2b7b6258cded92
LaKeisha P <lakeisharprice@gmail.com>
2021-03-03 14:27:14 -0500

    Create app3.js

---
+++ app3.js
@@ -0,0 +1,19 @@
+var game = {
+    win:"0", 
+    loss: "0"
+    };
+
+var choices = ["rock", "paper", & "scissors"];
+
+var random1 = 0;
+var random2 = 1;
+
+var bot1 = choices[random1];
+var bot2 = choices[random2];
+
+if(bot2>0){greeting='Congratulations! Bot2 wins';
+}else if (bot1<0){greeting="Congratulations! Bot1 wins";}
+else {greeting}
+
+var results = ""
+
